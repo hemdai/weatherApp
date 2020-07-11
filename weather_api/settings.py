b246f750a3c65fe27edcc712effa1e466a9c0257
hemdai <fogalop@gmail.com>
2020-07-11 10:51:26 +0200

    second

--- weather_api/settings.py
+++ weather_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fdmomb$qyhh@6x616a*k&9to92q84y&8-oom=k@9twhozl+%ja'
+SECRET_KEY = 'secretkey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
